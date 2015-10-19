c46bf5a88779d78b6e6da88743929974f0b47432
Koichi Tanaka <tanaka51.jp@gmail.com>
2015-10-20 01:09:17 +0900

    relationship and validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,7 @@
 class Page < ActiveRecord::Base
-  belongs_to :wiki
+  belongs_to :wiki, required: true
+
+  validates :blob, presence: true, length: { maximum: 255 }, uniqueness: { scope: %i(wiki_id) }
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :content, presence: true
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,2 +1,6 @@
 class Wiki < ActiveRecord::Base
+  has_many :pages, dependent: :destroy
+
+  validates :blob, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 255 }
 end
